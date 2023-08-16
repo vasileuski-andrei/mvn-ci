5cf1dd9813780e1506af8baef30f0fd922a8032c
Andrei_Vasileuski <andrei_vasileuski@senla.eu>
2023-08-16 17:40:25 +0300

    first commit

---
+++ src/main/java/com/example/mvnci/MvnCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvnci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvnci/MvnCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvnci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
